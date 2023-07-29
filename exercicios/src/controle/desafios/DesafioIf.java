f9ae509a6c2e39f12d9c38e901efe237543e04a5
vinizer4 <vinicius.ts.online@gmail.com>
2023-07-28 21:34:24 -0300

    feat: desafio if

---
+++ exercicios/src/controle/desafios/DesafioIf.java
@@ -0,0 +1,15 @@
+package controle.desafios;
+
+public class DesafioIf {
+
+    public static void main(String[] args) {
+
+        double nota = 1.3;
+
+        // nao usar ; em estruturas de controle (tem uma excecao)
+        if (nota >= 9.0) {
+            System.out.println("Quadro de Honra!");
+            System.out.println("Voce e fera!!");
+        }
+    }
+}
