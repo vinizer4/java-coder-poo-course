bcaf0800515cd23d0903995e9b83aba36ae4c51d
vinizer4 <vinicius.ts.online@gmail.com>
2023-07-29 15:35:47 -0300

    feat: desafio for 2

---
+++ exercicios/src/controle/desafios/DesafioForNum2.java
@@ -0,0 +1,15 @@
+package controle.desafios;
+
+public class DesafioForNum2 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.printf("[%d %d]", i, j);
+            }
+        }
+
+
+    }
+}
