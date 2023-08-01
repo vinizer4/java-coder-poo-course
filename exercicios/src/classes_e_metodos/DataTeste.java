1b1ff6cc2df122ec9de1cb4450e1635222f691f4
vinizer4 <vinicius.ts.online@gmail.com>
2023-08-01 15:45:19 -0300

    feat: implementando metodos na classe data

--- exercicios/src/classes_e_metodos/DataTeste.java
+++ exercicios/src/classes_e_metodos/DataTeste.java
@@ -10,6 +10,8 @@ public class DataTeste {
         d1.ano = 2021;
 
         System.out.printf("%d/%d/%d\n", d1.dia, d1.mes, d1.ano);
+        System.out.println(d1.obterDataFormatada());
+        d1.imprimirDataFormatada();
 
         var d2 = new Data();
 
@@ -18,5 +20,6 @@ public class DataTeste {
         d2.ano = 2020;
 
         System.out.printf("%d/%d/%d\n", d2.dia, d2.mes, d2.ano);
+        System.out.println(d2.obterDataFormatada());
     }
 }
