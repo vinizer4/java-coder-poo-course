5ca9453a5fc60b1cb0b276a817e53b00f93185b1
vinizer4 <vinicius.ts.online@gmail.com>
2023-08-02 00:10:59 -0300

    feat: valores padroes

--- exercicios/src/classes_e_metodos/Data.java
+++ exercicios/src/classes_e_metodos/Data.java
@@ -6,11 +6,26 @@ public class Data {
     int mes;
     int ano;
 
+    // final int x;
+    // no caso de constantes elas obrigatoriamente tem
+    // que ser definidas, nao recebem valor padrao esse exemplo lanca um erro
+    // variavel x nao foi inicializada!!!
+
     Data() {
        // dia = 1;
        // mes = 1;
        // ano = 1970;
         this(1, 1, 1970);
+
+        // byte, short, int, long -> valor padrao = 0
+        // float, double -> valor padrao = 0.0
+        // boolean -> valor padrao = false
+        // char -> valor padrao = '\u00000'
+        // Objetos -> valor padrao = null
+
+        // OBS: variavel local nao recebe valor padrao o codigo abaixo lanca um erro
+        // int a;
+        // System.out.println(a); variavel a nao foi inicializada!!!
     }
 
     Data(int dia, int mes, int ano) {
--- exercicios/src/classes_e_metodos/DataTeste.java
+++ exercicios/src/classes_e_metodos/DataTeste.java
@@ -5,6 +5,7 @@ public class DataTeste {
     public static void main(String[] args) {
 
         Data d1 = new Data();
+        d1.imprimirDataFormatada();
         //d1.ano = 2021;
 
         System.out.printf("%d/%d/%d\n", d1.dia, d1.mes, d1.ano);
