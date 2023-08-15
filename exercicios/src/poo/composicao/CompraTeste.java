cd997896b27aa9f1c16015737a094d7b60e94aaf
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-14 23:50:12 -0300

    feat: POO relacionamento um para muitos

---
+++ exercicios/src/poo/composicao/CompraTeste.java
@@ -0,0 +1,15 @@
+package poo.composicao;
+
+public class CompraTeste {
+
+    public static void main(String[] args) {
+
+        Compra compra1 = new Compra();
+        compra1.cliente = "João Pedro";
+        compra1.adicionarItem("Caneta", 20, 7.45);
+        compra1.adicionarItem(new Item("Borracha", 12, 3.89));
+        compra1.adicionarItem(new Item("Caderno", 3, 18.79));
+
+        System.out.println(compra1.itens.size());
+    }
+}
