797f7e3de8a7284e1c3acc3d28afefd4459108df
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-15 21:46:44 -0300

    feat: POO Heranca construtores desafio

--- exercicios/src/poo/heranca/desafio/Civic.java
+++ exercicios/src/poo/heranca/desafio/Civic.java
@@ -2,4 +2,7 @@ package poo.heranca.desafio;
 
 public class Civic extends Carro{
 
+    Civic() {
+        super(212);
+    }
 }
--- exercicios/src/poo/heranca/desafio/Ferrari.java
+++ exercicios/src/poo/heranca/desafio/Ferrari.java
@@ -2,6 +2,15 @@ package poo.heranca.desafio;
 
 public class Ferrari extends Carro{
 
+    Ferrari() {
+        this(315);
+    }
+
+    Ferrari(int velocidadeMaxima) {
+        super(velocidadeMaxima);
+        delta = 15;
+    }
+
     @Override
     void acelerar() {
         velocidadeAtual += 15;
