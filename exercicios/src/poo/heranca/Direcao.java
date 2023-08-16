d4769cb349a2c5d753c5978b6b4713af9083f339
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-15 21:10:40 -0300

    feat: POO Enum

---
+++ exercicios/src/poo/heranca/Direcao.java
@@ -0,0 +1,5 @@
+package poo.heranca;
+
+public enum Direcao {
+    NORTE, SUL, LESTE, OESTE;
+}
---
+++ exercicios/src/poo/heranca/Jogador.java
@@ -0,0 +1,26 @@
+package poo.heranca;
+
+public class Jogador {
+
+    int x;
+    int y;
+
+    boolean andar(Direcao direcao) {
+        switch (direcao) {
+            case NORTE:
+                y--;
+                break;
+            case SUL:
+                y++;
+                break;
+            case LESTE:
+                x++;
+                break;
+            case OESTE:
+                x--;
+                break;
+        }
+
+        return true;
+    }
+}
