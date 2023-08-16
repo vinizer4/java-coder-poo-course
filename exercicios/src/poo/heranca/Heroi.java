51c12cface65ca52a356b62c0c7a62f52971fbaf
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-15 21:21:52 -0300

    feat: POO Heranca substituicao metodos da classe pai na classe filha

--- exercicios/src/poo/heranca/Heroi.java
+++ exercicios/src/poo/heranca/Heroi.java
@@ -2,4 +2,9 @@ package poo.heranca;
 
 public class Heroi extends Jogador {
 
+    boolean atacar(Jogador oponente) {
+        boolean ataque1 = super.atacar(oponente);
+        boolean ataque2 = super.atacar(oponente);
+        return ataque1 || ataque2;
+    }
 }
