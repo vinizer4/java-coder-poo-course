8f610e680154253fc832fd450ecc1c9c2e501261
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-16 08:30:20 -0300

    feat: Lambdas => interface funcional

--- exercicios/src/lambdas/Calculo.java
+++ exercicios/src/lambdas/Calculo.java
@@ -1,5 +1,6 @@
 package lambdas;
 
+@FunctionalInterface // opcional mas recomendado para interfaces funcionais (com apenas um método) lambdas
 public interface Calculo {
 
     double executar(double a, double b);
