a6541afec2835cd29db4fb3bd45ce708975a5783
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-16 23:44:52 -0300

    feat: Streams API => Filter desafio

--- exercicios/src/stream/Aluno.java
+++ exercicios/src/stream/Aluno.java
@@ -4,9 +4,11 @@ public class Aluno {
 
     final String nome;
     final double nota;
+    final boolean bomComportamento;
 
-    public Aluno(String nome, double nota) {
+    public Aluno(String nome, double nota, boolean bomComportamento) {
         this.nome = nome;
         this.nota = nota;
+        this.bomComportamento = bomComportamento;
     }
 }
