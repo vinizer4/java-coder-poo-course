b79d66c5f8a20177edae95aa0185e1a661bd9cf0
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-18 18:01:08 -0300

    feat: Campo minado => iniciado classe Tabuleiro

--- campo-minado/src/main/java/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ campo-minado/src/main/java/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -1,4 +1,52 @@
 package br.com.cod3r.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+
+    private int linhas;
+    private int colunas;
+    private int minas;
+
+    private final List<Campo> campos = new ArrayList<>();
+
+    public Tabuleiro(int linhas, int colunas, int minas) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+
+        gerarCampos();
+        associarOsVizinhos();
+        sortearMinas();
+    }
+
+    private void gerarCampos() {
+        for (int linha = 0; linha < linhas; linha++) {
+            for (int coluna = 0; coluna < colunas; coluna++) {
+                campos.add(new Campo(linha, coluna));
+            }
+        }
+    }
+
+    private void associarOsVizinhos() {
+        for (Campo c1 : campos) {
+            for (Campo c2 : campos) {
+                c1.adicionarVizinho(c2);
+            }
+        }
+    }
+
+    private void sortearMinas() {
+        for (int i = 0; i < minas; i++) {
+            int aleatorio = (int) (Math.random() * campos.size());
+            campos.get(aleatorio).minar();
+        }
+    }
+
+
+
+
+
+
 }
