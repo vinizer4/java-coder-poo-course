c50935292a77c606dcc98666a0cfaf2ce70927da
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-18 15:32:53 -0300

    feat: Exceptions => Excecoes personalizadas

---
+++ exercicios/src/excecao/personalizadaA/StringVaziaException.java
@@ -0,0 +1,14 @@
+package excecao.personalizadaA;
+
+public class StringVaziaException extends RuntimeException {
+
+    private String nomeDoAtributo;
+
+    public StringVaziaException(String nomeDoAtributo) {
+        this.nomeDoAtributo = nomeDoAtributo;
+    }
+
+    public String getMessage() {
+        return String.format("O atributo '%s' está vazio.", nomeDoAtributo);
+    }
+}
