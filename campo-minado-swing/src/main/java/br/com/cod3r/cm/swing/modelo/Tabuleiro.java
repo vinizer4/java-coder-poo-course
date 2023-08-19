88da6224e43c561e17ff1cc8147ceb656e22aace
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-19 14:29:46 -0300

    feat: Java Desktop => atualizacao classe tabuleiro

--- campo-minado-swing/src/main/java/br/com/cod3r/cm/swing/modelo/Tabuleiro.java
+++ campo-minado-swing/src/main/java/br/com/cod3r/cm/swing/modelo/Tabuleiro.java
@@ -2,15 +2,17 @@ package br.com.cod3r.cm.swing.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
-public class Tabuleiro {
+public class Tabuleiro implements CampoObservador {
 
     private int linhas;
     private int colunas;
     private int minas;
 
     private final List<Campo> campos = new ArrayList<>();
+    private final List<Consumer<ResultadoEvento>> observadores = new ArrayList<>();
 
     public Tabuleiro(int linhas, int colunas, int minas) {
         this.linhas = linhas;
@@ -20,6 +22,15 @@ public class Tabuleiro {
         gerarCampos();
         associarOsVizinhos();
         sortearMinas();
+    }
+
+    public void registrarObservador(Consumer<ResultadoEvento> observador) {
+        observadores.add(observador);
+    }
+
+    private void notificarObservadores(boolean resultado) {
+        observadores.stream()
+                .forEach(o -> o.accept(new ResultadoEvento(resultado)));
     }
 
     public void abrir(int linha, int coluna) {
@@ -45,7 +56,9 @@ public class Tabuleiro {
     private void gerarCampos() {
         for (int linha = 0; linha < linhas; linha++) {
             for (int coluna = 0; coluna < colunas; coluna++) {
-                campos.add(new Campo(linha, coluna));
+                Campo campo = new Campo(linha, coluna);
+                campo.registrarObservador(this);
+                campos.add(campo);
             }
         }
     }
@@ -76,6 +89,23 @@ public class Tabuleiro {
     public void reiniciar() {
         campos.stream().forEach(c -> c.reiniciar());
         sortearMinas();
+    }
+
+    @Override
+    public void eventoOcorreu(Campo campo, CampoEvento evento) {
+        if (evento == CampoEvento.EXPLODIR) {
+            mostrarMinas();
+            notificarObservadores(false);
+        } else if (objetivoAlcancado()) {
+            notificarObservadores(true);
+        }
+    }
+
+    private void mostrarMinas() {
+        campos.stream()
+                .filter(campo -> campo.isMinado())
+                .filter(campo -> !campo.isMarcado())
+                .forEach(campo -> campo.setAberto(true));
     }
 
 }
