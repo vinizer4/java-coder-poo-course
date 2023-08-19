921222fb8e33fcc788204dde6d3fa71b51cba29c
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-18 22:39:32 -0300

    feat: Generics => trabalhando com tipos genericos com herancas na definicao dos tipos

---
+++ exercicios/src/generics/CaixaNumeroTeste.java
@@ -0,0 +1,19 @@
+package generics;
+
+public class CaixaNumeroTeste {
+
+    public static void main(String[] args) {
+
+        CaixaNumero<Double> caixaA = new CaixaNumero<>();
+        caixaA.guardar(2.3);
+
+        Double coisaA = caixaA.abrir();
+        System.out.println(coisaA);
+
+        CaixaNumero<Integer> caixaB = new CaixaNumero<>();
+        caixaB.guardar(123);
+
+        Integer coisaB = caixaB.abrir();
+        System.out.println(coisaB);
+    }
+}
