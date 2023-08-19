3a72005a778e471b4e19f6cec518b181a2fdf79e
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-19 13:58:35 -0300

    feat: Java Desktop => configuracao inicial projeto campo minado com swing

---
+++ campo-minado-swing/src/test/java/Teste.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Teste {
+
+    @Test
+    void verificarJUnit() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void testarSeIgualADois() {
+        int numero = 2;
+
+        Assertions.assertEquals(2, numero);
+    }
+
+}
