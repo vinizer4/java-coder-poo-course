631a6aea71f7d8855ce9457df6530c192bdf2879
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-24 13:54:29 -0300

    feat: Java Desktop => implementando logica para identificar os comandos

--- calculado-swing/src/main/java/br/com/cod3r/calc/modelo/Memoria.java
+++ calculado-swing/src/main/java/br/com/cod3r/calc/modelo/Memoria.java
@@ -2,8 +2,13 @@ package br.com.cod3r.calc.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Memoria {
+
+    private enum TipoComando {
+        ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA, AC
+    }
 
     private static final Memoria instancia = new Memoria();
 
@@ -28,13 +33,49 @@ public class Memoria {
         return textoAtual.isEmpty() ? "0" : textoAtual;
     }
 
-    public void processarComando(String valor) {
-        if ("AC".equals(valor)) {
+    public void processarComando(String texto) {
+
+        TipoComando tipoComando = detectarTipoComando(texto);
+        System.out.println(tipoComando);
+
+        if ("AC".equals(texto)) {
             textoAtual = "";
         } else {
-            textoAtual += valor;
+            textoAtual += texto;
         }
 
         observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+    }
+
+    private TipoComando detectarTipoComando(String texto) {
+
+        if (textoAtual.isEmpty() && Objects.equals(texto, "0")) {
+            return null;
+        }
+
+        try {
+            Integer.parseInt(texto);
+            return TipoComando.NUMERO;
+        } catch (NumberFormatException e) {
+            // Quando não for número...
+
+            if ("AC".equals(texto)) {
+                return TipoComando.AC;
+            } else if ("/".equals(texto)) {
+                return TipoComando.DIV;
+            } else if ("*".equals(texto)) {
+                return TipoComando.MULT;
+            } else if ("+".equals(texto)) {
+                return TipoComando.SOMA;
+            } else if ("-".equals(texto)) {
+                return TipoComando.SUB;
+            } else if ("=".equals(texto)) {
+                return TipoComando.IGUAL;
+            } else if (",".equals(texto) && !textoAtual.contains(",")) {
+                return TipoComando.VIRGULA;
+            }
+        }
+
+        return null;
     }
 }
