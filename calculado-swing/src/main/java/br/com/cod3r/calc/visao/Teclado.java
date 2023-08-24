345ef080e7e6ea981ef3bcd3f112f10a729db9ea
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-24 09:08:01 -0300

    feat: Java Desktop => ajustando layout dos botoes

--- calculado-swing/src/main/java/br/com/cod3r/calc/visao/Teclado.java
+++ calculado-swing/src/main/java/br/com/cod3r/calc/visao/Teclado.java
@@ -5,6 +5,10 @@ import java.awt.*;
 
 public class Teclado extends JPanel {
 
+    private final Color COR_CINZA_ESCURO = new Color(68, 68, 68);
+    private final Color COR_CINZA_CLARO = new Color(99, 99, 99);
+    private final Color COR_LARANJA = new Color(242, 163, 60);
+
     public Teclado() {
 
     }
