fe7b47a39c87b360c7d58ce503e0f6677f95ef43
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-24 09:01:04 -0300

    feat: Java Desktop => ajustando layout do display

--- calculado-swing/src/main/java/br/com/cod3r/calc/visao/Display.java
+++ calculado-swing/src/main/java/br/com/cod3r/calc/visao/Display.java
@@ -5,7 +5,18 @@ import java.awt.*;
 
 public class Display extends JPanel {
 
+    private final JLabel label;
+
     public Display() {
+
+        setBackground(new Color(46, 49, 50));
+        label = new JLabel("1234,56");
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("courier", Font.PLAIN, 30));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+
+        add(label);
 
     }
 }
