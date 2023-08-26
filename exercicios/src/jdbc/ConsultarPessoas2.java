584d297ef2b7ec550db09221e409ab9ba78702bb
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-26 18:01:13 -0300

    feat: JDBC => consultar pessoas usando like

---
+++ exercicios/src/jdbc/ConsultarPessoas2.java
@@ -0,0 +1,40 @@
+package jdbc;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ConsultarPessoas2 {
+
+    public static void main(String[] args) throws SQLException {
+        Scanner entrada = new Scanner(System.in);
+
+        Connection conexao = FabricaConexao.getConexao();
+        String sql = "SELECT * FROM pessoas WHERE nome LIKE ?";
+
+        System.out.println("Informe o valor para pesquisa: ");
+        String valor = entrada.nextLine();
+
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+        stmt.setString(1, "%" + valor + "%");
+
+        ResultSet resultado = stmt.executeQuery();
+
+        List<Pessoa> pessoas = new ArrayList<>();
+
+        while (resultado.next()) {
+            int codigo = resultado.getInt("codigo");
+            String nome = resultado.getString("nome");
+            pessoas.add(new Pessoa(codigo, nome));
+        }
+
+        for (Pessoa p : pessoas) {
+            System.out.println(p.getCodigo() + " ==> " + p.getNome());
+        }
+
+        stmt.close();
+        conexao.close();
+
+    }
+}
