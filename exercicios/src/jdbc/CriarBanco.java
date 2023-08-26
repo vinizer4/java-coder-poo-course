34a63cac273ea1df3c710c71639684faa38fc7ca
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-08-26 17:38:59 -0300

    feat: JDBC => criado tabela

--- exercicios/src/jdbc/CriarBanco.java
+++ exercicios/src/jdbc/CriarBanco.java
@@ -8,20 +8,20 @@ import java.sql.Statement;
 public class CriarBanco {
 
     public static void main(String[] args) throws SQLException {
-        final String url = "jdbc:mysql://localhost:33066?verifyServerCertificate=false&useSSL=true";
-        final String usuario = "admin";
-        final String senha = "admin";
+//        final String url = "jdbc:mysql://localhost:33066?verifyServerCertificate=false&useSSL=true";
+//        final String usuario = "admin";
+//        final String senha = "admin";
+//
+//        Connection conexao = DriverManager
+//                .getConnection(url, usuario, senha);
 
-        Connection conexao = DriverManager
-                .getConnection(url, usuario, senha);
+        Connection conexao = FabricaConexao.getConexao();
 
         Statement stmt = conexao.createStatement();
 
         stmt.execute("CREATE DATABASE IF NOT EXISTS curso_java");
 
         System.out.println("Banco criado com sucesso!");
-
-        System.out.println("Conexão efetuada com sucesso!");
 
         conexao.close();
     }
