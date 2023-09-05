85ac23482443adb1d8d3f1bbbc0b0eaeec1b6cd3
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-09-04 23:52:55 -0300

    feat: JPA => entidade usuario

---
+++ exercicios-jpa/src/main/model/basic/Usuario.java
@@ -0,0 +1,45 @@
+package basic;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+
+    @Id
+    private long id;
+    private String nome;
+    private String email;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
