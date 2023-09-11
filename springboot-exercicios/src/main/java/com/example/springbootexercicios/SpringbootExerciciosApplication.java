1ad51895033801dfc32373f021baad04f3f4273d
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-09-11 13:49:56 -0300

    feat: SpringBoot => Criando projeto

---
+++ springboot-exercicios/src/main/java/com/example/springbootexercicios/SpringbootExerciciosApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootexercicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExerciciosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootExerciciosApplication.class, args);
+    }
+
+}
---
+++ springboot-exercicios/src/test/java/com/example/springbootexercicios/SpringbootExerciciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootexercicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExerciciosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
