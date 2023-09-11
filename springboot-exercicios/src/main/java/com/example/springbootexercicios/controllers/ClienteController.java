dc9507ca5da31b8a22800e9d822ca06820142917
Vinicius Teixeira Saraiva <vinicius.saraiva@grupoboticario.com.br>
2023-09-11 14:43:06 -0300

    feat: SpringBoot => Path variable

--- springboot-exercicios/src/main/java/com/example/springbootexercicios/controllers/ClienteController.java
+++ springboot-exercicios/src/main/java/com/example/springbootexercicios/controllers/ClienteController.java
@@ -2,6 +2,7 @@ package com.example.springbootexercicios.controllers;
 
 import com.example.springbootexercicios.models.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class ClienteController {
     @GetMapping("/obter")
     public Cliente obterCliente() {
         return new Cliente(1, "Fulano", "123.456.789-00");
+    }
+
+    @GetMapping("/{id}")
+    public Cliente obterClientePorId(@PathVariable int id) {
+        return new Cliente(id, "Fulano", "123.456.789-00");
     }
 }
